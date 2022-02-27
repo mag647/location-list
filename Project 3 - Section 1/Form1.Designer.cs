bd7275eb9cfd26e6e832c9b82692aa9e98587d8f
mag647 <michele.a.gay@gmail.com>
2022-02-27 17:07:33 -0500

    Add project files.

---
+++ Project 3 - Section 1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Project_3___Section_1
+{
+    partial class startupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.locationListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.exportDelimButton = new System.Windows.Forms.Button();
+            this.exportFixedWidthButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.letterComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // locationListBox
+            // 
+            this.locationListBox.FormattingEnabled = true;
+            this.locationListBox.Location = new System.Drawing.Point(46, 49);
+            this.locationListBox.Name = "locationListBox";
+            this.locationListBox.Size = new System.Drawing.Size(148, 186);
+            this.locationListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CDC Locations";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(223, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Filter List Using Letter";
+            // 
+            // exportDelimButton
+            // 
+            this.exportDelimButton.Location = new System.Drawing.Point(255, 133);
+            this.exportDelimButton.Name = "exportDelimButton";
+            this.exportDelimButton.Size = new System.Drawing.Size(172, 23);
+            this.exportDelimButton.TabIndex = 3;
+            this.exportDelimButton.Text = "Export to &Delimited File (.csv)";
+            this.exportDelimButton.UseVisualStyleBackColor = true;
+            this.exportDelimButton.Click += new System.EventHandler(this.exportDelimButton_Click);
+            // 
+            // exportFixedWidthButton
+            // 
+            this.exportFixedWidthButton.Location = new System.Drawing.Point(255, 171);
+            this.exportFixedWidthButton.Name = "exportFixedWidthButton";
+            this.exportFixedWidthButton.Size = new System.Drawing.Size(172, 23);
+            this.exportFixedWidthButton.TabIndex = 4;
+            this.exportFixedWidthButton.Text = "Export to &Fixed Width File (.txt)";
+            this.exportFixedWidthButton.UseVisualStyleBackColor = true;
+            this.exportFixedWidthButton.Click += new System.EventHandler(this.exportFixedWidthButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(352, 212);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // letterComboBox
+            // 
+            this.letterComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.letterComboBox.FormattingEnabled = true;
+            this.letterComboBox.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.letterComboBox.Location = new System.Drawing.Point(226, 49);
+            this.letterComboBox.Name = "letterComboBox";
+            this.letterComboBox.Size = new System.Drawing.Size(57, 21);
+            this.letterComboBox.TabIndex = 6;
+            this.letterComboBox.SelectedIndexChanged += new System.EventHandler(this.letterComboBox_SelectedIndexChanged);
+            // 
+            // startupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 271);
+            this.Controls.Add(this.letterComboBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.exportFixedWidthButton);
+            this.Controls.Add(this.exportDelimButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.locationListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "startupForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "P3S1 Data File Tasks";
+            this.Load += new System.EventHandler(this.startupForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox locationListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button exportDelimButton;
+        private System.Windows.Forms.Button exportFixedWidthButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ComboBox letterComboBox;
+    }
+}
+
---
+++ Project 3 - Section 1/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project_3___Section_1
+{
+    // Author: Michele Gay
+    // Created Date: 05/05/2020
+    // Purpose: Allows the user to select a group of countries from a list box
+    //          by letter. This group can then be exported to a fixed-width 
+    //          (.txt) or comma-delimited (.csv) file. 
+
+    public partial class startupForm : Form
+    {
+        public startupForm()
+        {
+            InitializeComponent();
+        }
+
+        List<string> locationList = new List<string>();
+        List<string> searchList = new List<string>();
+
+        private void exportDelimButton_Click(object sender, EventArgs e)
+        {
+            const String EXPORTED_FILE_NAME = "\\" + "P3S1 Comma Delimited Export.csv";
+            int curLocationInteger = 0;
+
+            try
+            {
+                // Create a file and get a StreamWriter object. 
+                StreamWriter selectedLocationsFile;
+
+                selectedLocationsFile = File.CreateText(AppDomain.CurrentDomain.BaseDirectory + EXPORTED_FILE_NAME);
+
+                // Write message to file if no countries are in the listbox. 
+                if (locationListBox.Items.Count == 0)
+                {
+                    selectedLocationsFile.Write("RECORD CRITERIA HAS 0 MATCHES");
+                }
+                // Write the location(s) from the listbox to the file. Add quotation marks and commas between each item. 
+                else
+                {
+                    while (curLocationInteger < locationListBox.Items.Count)
+                    {
+                        if (curLocationInteger <= (locationListBox.Items.Count - 2))
+                        {
+                            selectedLocationsFile.Write("\"" + locationListBox.Items[curLocationInteger].ToString() + "\",");
+                            curLocationInteger = curLocationInteger + 1;
+                        }
+                        else if (curLocationInteger == (locationListBox.Items.Count - 1))
+                        {
+                            selectedLocationsFile.Write("\"" + locationListBox.Items[curLocationInteger].ToString() + "\"");
+                            curLocationInteger = curLocationInteger + 1;
+                        }
+
+                    }
+                }
+
+                // Close the file and confirm the process has completed.
+                selectedLocationsFile.Close();
+                MessageBox.Show("Location(s) file exported.  Click \"OK\" to continue.");
+            }
+            catch (Exception GenericException)
+            {
+                MessageBox.Show(GenericException.Message);
+                return;
+            }
+        }
+
+        private void exportFixedWidthButton_Click(object sender, EventArgs e)
+        {
+            const String EXPORTED_FILE_NAME = "\\" + "P3S1 Fixed Width Export.txt";
+            const int TOTAL_SPACE_AVAILABLE = 15;
+            int curLocationInteger = 0;
+
+            try
+            {
+                // Create a file and get a StreamWriter object. 
+                StreamWriter selectedLocationsFile;
+                selectedLocationsFile = File.CreateText(AppDomain.CurrentDomain.BaseDirectory + EXPORTED_FILE_NAME);
+
+                if (locationListBox.Items.Count == 0)
+                {
+                    selectedLocationsFile.Write("THE CRITERIA HAS NO MATCHES");
+                }
+                else
+                {
+                    while (curLocationInteger < locationListBox.Items.Count)
+                    {
+                        string country = locationListBox.Items[curLocationInteger].ToString();
+                        int countryLength = country.Length;
+                        int numSpaces = TOTAL_SPACE_AVAILABLE - country.Length;
+                        selectedLocationsFile.WriteLine(locationListBox.Items[curLocationInteger].ToString() + String.Empty.PadRight(numSpaces, ' '));
+                        curLocationInteger = curLocationInteger + 1;
+                    }
+                }
+                
+                // Close the file and confirm the process has completed.
+                selectedLocationsFile.Close();
+                MessageBox.Show("Location(s) file exported.  Click \"OK\" to continue.");
+            }
+            catch (Exception GenericException)
+            {
+                MessageBox.Show(GenericException.Message);
+                return;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Confirm the form exit by prompting the user first.
+            if (MessageBox.Show("Are you sure you want to exit?",
+                "Confirm exit...",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void startupForm_Load(object sender, EventArgs e)
+        {
+            letterComboBox.SelectedIndex = 2;
+
+            try
+            {
+                string locationNameString;
+                StreamReader p3S1InputDataFile;
+                int cycleCount = 0;
+
+                //Open the file and get a StreamReader object.
+                p3S1InputDataFile = File.OpenText("P3S1 Data File For Import.txt");
+
+                //Read the input file's contents until end of stream.
+                while (!p3S1InputDataFile.EndOfStream)
+                {
+                    locationNameString = p3S1InputDataFile.ReadLine();
+                    cycleCount++;
+
+                    // Exclude first two entries.
+                    if (cycleCount >= 3)
+                    {
+                        //Declares new line as a delimiter. 
+                        char[] delimiter = new char[] { '\n' };
+
+                        //Declares an array countries
+                        var countries = locationNameString.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (object country in countries)
+                        {
+                            locationListBox.Items.Add(country.ToString());
+                            searchList.Add(country.ToString());
+                        }
+                    }
+                }
+
+                // Close the file and confirm the process has completed.
+                p3S1InputDataFile.Close();  
+            }
+            catch (Exception genericException)
+            {
+                MessageBox.Show(genericException.Message);
+                return;
+            }
+        }
+
+        private void letterComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            locationList.Clear();
+            string selLetter = letterComboBox.SelectedItem.ToString();
+
+            for (int i = 0; i < searchList.Count; i++)
+            {
+                //If a country starts with the selected letter, add to the locationList.
+                string curElement = searchList.ElementAt(i).ToString();
+                char firstLetter = curElement[0];
+                string firstLet = firstLetter.ToString();
+                if (firstLet == selLetter)
+                {
+                    locationList.Add(searchList.ElementAt(i).ToString());
+
+                }
+            }
+
+            locationListBox.Items.Clear();
+            
+            //Display items from locationList in the list box. 
+            foreach (string str in locationList)
+            {
+                locationListBox.Items.Add(str);
+            }
+        }
+    }
+}
